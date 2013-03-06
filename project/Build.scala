282beae5848dfbc1b4b48a0d9f722ad50a8c765d
Tim Dettrick <t.dettrick@uq.edu.au>
2013-03-06 10:06:47 +1000

    Adding jclouds dependencies for file storage.

--- project/Build.scala
+++ project/Build.scala
@@ -7,12 +7,15 @@ object ApplicationBuild extends Build {
   val appName         = "aorra"
   val appVersion      = "1.0-SNAPSHOT"
 
+  def jcloudsDep(s: String) = { "org.jclouds.api" % s % "1.5.7" }
+
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    jcloudsDep("filesystem"),
+    jcloudsDep("swift")
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
