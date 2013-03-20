d390b72825f826d65937a2aa64af689c4beb98eb
Tim Dettrick <t.dettrick@uq.edu.au>
2013-03-20 10:45:14 +1000

    Refactored FileUploadController to use Play JSON support.
    Also took advantage of Scala to handle bad requests.

--- app/controllers/FileUploadController.scala
+++ app/controllers/FileUploadController.scala
@@ -5,6 +5,7 @@ import java.util.Iterator
 import java.util.List
 import play.Logger
 import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
 import securesocial.core.SecureSocial
 
@@ -12,34 +13,29 @@ object FileUploadController extends Controller with SecureSocial {
 
   def postUpload = SecuredAction { implicit request =>
     val body = request.body.asMultipartFormData
-    val buf = new StringBuilder("{\"files\": [")
-    val files = body.get.files // TODO: Handle no body
-    // TODO: Do this without a while loop
-    val iter = files.iterator
-    while (iter.hasNext) {
-      val filePart = iter.next
-      val fileName = filePart.filename
-      val file = filePart.ref.file
-      // TODO: Consider using JsonHelper
-      buf.append("{")
-      buf.append(s""""name": "$fileName",""")
-      buf.append(s""""size": "${file.length}"""")
-      buf.append("}")
-      if (iter.hasNext) {
-        buf.append(",")
-      }
-      Logger.info(String.format("file %s content type %s uploaded to %s",
-        fileName, filePart.contentType, file.getAbsolutePath()))
+    body match {
+      case Some(body) =>
+        val json = Json.obj(
+          "files" -> body.files.map { filePart =>
+            val fileName = filePart.filename
+            val file = filePart.ref.file
+            Logger.info(String.format("file %s content type %s uploaded to %s",
+              fileName, filePart.contentType, file.getAbsolutePath()))
+            Json.obj(
+              "name" -> fileName,
+              "size" -> file.length)
+          })
+        // even though we return json set the content type to text/html
+        // to prevent IE/Opera from opening a download dialog as described here:
+        // https://github.com/blueimp/jQuery-File-Upload/wiki/Setup
+        Ok(json).as("text/html")
+      case None =>
+        BadRequest("POST must contain multipart form data.").as("text/plain")
     }
-    buf.append("]}");
-    // even though we return json set the content type to text/html to prevent IE/Opera
-    // from opening a download dialog as described here:
-    // https://github.com/blueimp/jQuery-File-Upload/wiki/Setup
-    Ok(buf.toString()).as("text/html")
   }
 
   def getUpload = SecuredAction { implicit request =>
-    Ok(views.html.upload.render())
+    Ok(views.html.upload.render)
   }
 
 }
