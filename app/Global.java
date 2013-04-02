a1c2e9e6dd443cb307fee76ad71a1470e1a1eda7
Tim Dettrick <t.dettrick@uq.edu.au>
2013-04-03 08:48:45 +1000

    Fixing compilation and tests after removing FreeformFileStore plugin.

--- app/Global.java
+++ app/Global.java
@@ -1,18 +1,19 @@
+import static play.Play.application;
+
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+
+import play.GlobalSettings;
+import service.JackrabbitUserService;
+import service.JcrSessionFactory;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Provides;
 import com.wingnest.play2.jackrabbit.Jcr;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
-import play.GlobalSettings;
-import service.JcrSessionFactory;
-import service.ContributionFolderProvider;
-import service.FreeformFileStore;
-import service.JackrabbitUserService;
 import com.wingnest.play2.jackrabbit.plugin.ConfigConsts;
-import static play.Play.application;
 
 public class Global extends GlobalSettings {
 
@@ -36,12 +37,6 @@ public class Global extends GlobalSettings {
       JackrabbitUserService getUserService() {
         return application().plugin(JackrabbitUserService.class);
       }
-
-      @Provides
-      ContributionFolderProvider getContributionFolderProvider() {
-        return application().plugin(FreeformFileStore.class);
-      }
-
 
     };
     Module sessionModule = new AbstractModule() {
--- test/service/filestore/FileStoreTest.java
+++ test/service/filestore/FileStoreTest.java
@@ -16,21 +16,23 @@ import play.libs.F.Function;
 import service.JcrSessionFactory;
 
 public class FileStoreTest {
-  @Test 
+  @Test
   public void canGetFolders() {
     running(fakeJavaApp(), new Runnable() {
+      @Override
       public void run() {
-        final JcrSessionFactory sessionFactory = 
+        final JcrSessionFactory sessionFactory =
             injector().getInstance(JcrSessionFactory.class);
         final FileStore fileStore = new FileStore(sessionFactory);
         sessionFactory.inSession(new Function<Session,FileStore.Folder>() {
+          @Override
           public FileStore.Folder apply(Session session) {
             try {
-              Set<FileStore.Folder> folders = 
+              Set<FileStore.Folder> folders =
                   fileStore.getManager(session).getFolders();
               assertThat(folders).hasSize(1);
               FileStore.Folder folder = folders.iterator().next();
-              assertThat(folder.getPath()).isEqualTo("/");
+              assertThat(folder.getPath()).isEqualTo("");
               return folder;
             } catch (RepositoryException e) {
               throw new RuntimeException(e);
@@ -40,5 +42,5 @@ public class FileStoreTest {
       }
     });
   }
-  
+
 }
