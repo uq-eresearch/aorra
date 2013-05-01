75d13777ce748afed3ae7bd03308d7dc4f7c84d6
Tim Dettrick <t.dettrick@uq.edu.au>
2013-05-01 15:23:13 +1000

    Adding handling of deletion events.
    Will require more work to handle deletion of child nodes.

--- app/assets/js/filemanager.js
+++ app/assets/js/filemanager.js
@@ -16,7 +16,26 @@
     return 'file';
   };
 
-  var FileTree = Backbone.Model.extend({
+  var FileTree = Backbone.View.extend({
+    tagName: "div",
+    render: function() {
+      var tree = glyphtree(this.$el, this.options);
+      selectHandler = _.bind(function(event, node) {
+        var m = _.defaults({}, node.attributes);
+        // Emit select event
+        if (node.type == 'folder') {
+          this.trigger("folder:select", m);
+        } else {
+          this.trigger("file:select", m);
+        }
+        // Show the active node on the tree
+        $('.label.label-info').removeClass('label label-info');
+        $(node.element()).children('.glyphtree-node-label')
+          .addClass('label label-info');
+      }, this);
+      tree.events.label.click = [selectHandler];
+      this.tree = tree;
+    },
     options: {
       startExpanded: true,
       typeResolver: function(struct) {
@@ -79,93 +98,73 @@
     }
   });
   
-  function catchErrors(f) {
-    return function(struct) { try { f.apply(this, arguments); } catch (e) {} };
-  }
-  
-  var NotificationFeed = Backbone.View.extend({
-    
-    url: '/notifications',
-    
-    open: function() {
-      var tree = _.bind(function() { return this.model.tree }, this);
-      var trigger = _.bind(this.trigger, this);
-      var eventHandlers = {
-        ping:   function(message) { /*console.log(message);*/ },
-        load:   function(struct) { 
-          trigger("event:load", struct);
-          tree().load(struct); 
-        },
-        create: catchErrors(function(struct) { tree().add(struct, struct.parentId) }),
-        update: catchErrors(function(struct) { tree().update(struct) }),
-        'delete': catchErrors(function(struct) { tree().remove(struct.id) })
-      };
-      
-      // Are we using a modern browser, or are we using IE?
-      if (typeof(window.EventSource) == 'undefined') {
-        // HTML iframe
-        function connect_htmlfile(url, callback) {
-          var ifrDiv = document.createElement("div");
-          var iframe = document.createElement("iframe");
-          ifrDiv.setAttribute("style", "display: none");
-          document.body.appendChild(ifrDiv);
-          ifrDiv.appendChild(iframe);
-          iframe.src = url;
-          iframe.onload = function() { iframe.currentWindow.location.reload(); };
-          // From: http://davidwalsh.name/window-iframe
-          function eventHandler(w, eventName, handler) {
-            var eMethod = w.addEventListener ? "addEventListener" : "attachEvent";
-            var eName = w.addEventListener ? eventName : "on" + eventName;
-            return w[eMethod](eName, handler, false);
+  var NotificationFeed = function(config) {
+      var obj = _.extend({}, config)
+      _.extend(obj, Backbone.Events);
+      _.extend(obj, {
+      url: '/notifications',
+      open: function() {
+        var trigger = _.bind(this.trigger, this);
+        // Are we using a modern browser, or are we using IE?
+        if (typeof(window.EventSource) == 'undefined') {
+          // HTML iframe
+          function connect_htmlfile(url, callback) {
+            var ifrDiv = document.createElement("div");
+            var iframe = document.createElement("iframe");
+            ifrDiv.setAttribute("style", "display: none");
+            document.body.appendChild(ifrDiv);
+            ifrDiv.appendChild(iframe);
+            iframe.src = url;
+            iframe.onload = function() { iframe.currentWindow.location.reload(); };
+            // From: http://davidwalsh.name/window-iframe
+            function eventHandler(w, eventName, handler) {
+              var eMethod = w.addEventListener ? "addEventListener" : "attachEvent";
+              var eName = w.addEventListener ? eventName : "on" + eventName;
+              return w[eMethod](eName, handler, false);
+            }
+            // Receive messages from iframe
+            eventHandler(window, 'message', function(e) {
+              var msg = JSON.parse(e.data);
+              callback(msg.type, msg.data);
+            });
           }
-          // Receive messages from iframe
-          eventHandler(window, 'message', function(e) {
-            var msg = JSON.parse(e.data);
-            callback(msg.type, msg.data);
+          connect_htmlfile(this.url, function(eventType, data) {
+            trigger('event:'+eventType, data);
           });
-        }
-        connect_htmlfile(this.url, function(eventType, data) {
-          eventHandlers[eventType](data);
-        });
-      } else {
-        // EventSource
-        var es = new EventSource(this.url);
-        es.addEventListener('ping', function(event) {
-          eventHandlers.ping(event.data);
-        });
-        $.each(['load', 'create', 'update', 'delete'], function(i, n) {
-          es.addEventListener(n, function(event) {
-            var struct = JSON.parse(event.data);
-            eventHandlers[n](struct);
-          });
-        });
-        this.es = es;
-      }
-    }
-  });
-  
-  var FileTreePane = Backbone.View.extend({
-    tagName: "div",
-    render: function() {
-      var tree = glyphtree(this.$el, this.model.options);
-      selectHandler = _.bind(function(event, node) {
-        var m = _.defaults({}, node.attributes);
-        // Emit select event
-        if (node.type == 'folder') {
-          this.trigger("folder:select", m);
         } else {
-          this.trigger("file:select", m);
+          // EventSource
+          var es = new EventSource(this.url);
+          es.addEventListener('ping', function(event) {
+            trigger('event:ping', event.data);
+          });
+          _.each(['load', 'create', 'update', 'delete'], function(n) {
+            es.addEventListener(n, function(event) {
+              var struct = JSON.parse(event.data);
+              trigger('event:'+n, struct);
+            });
+          });
+          this.es = es;
         }
-        // Show the active node on the tree
-        $('.label.label-info').removeClass('label label-info');
-        $(node.element()).children('.glyphtree-node-label')
-          .addClass('label label-info');
-      }, this);
-      tree.events.label.click = [selectHandler];
-      this.model.tree = tree;
+      }
+    });
+    
+    function catchErrors(f) {
+      return function(struct) { try { f.apply(this, arguments); } catch (e) {} };
     }
-  });
-  
+    
+    var tree = _.bind(obj.getTree, this);
+    // Event handlers
+    obj.on("event:load", function(struct) { tree().load(struct); });
+    obj.on("event:create",
+      catchErrors(function(struct) { tree().add(struct, struct.parentId) }));
+    obj.on("event:update",
+      catchErrors(function(struct) { tree().update(struct) }));
+    obj.on("event:delete",
+      catchErrors(function(struct) { tree().remove(struct.id) }));
+    
+    return obj;
+  };
+    
   var FileUploadView = Backbone.View.extend({
     url: '/filestore',
     tagName: 'div',
@@ -345,6 +344,22 @@
     }
   });
   
+  var DeletedView = Backbone.View.extend({
+    initialize: function() { this.render(); },
+    render: function() {
+      var $heading = $('<h1/>')
+        .css('text-align', 'center');
+      var $symbol = $('<span/>')
+        .css('font-size', '5em')
+        .addClass("muted")
+        .html('<i class="icon-remove-circle"></i>');
+      var $message = $('<small/>');
+      $message.text("This location has been deleted.");
+      $heading.append($symbol, '<br />', $message);
+      this.$el.append($heading)
+    }
+  });
+  
   var MainPane = Backbone.View.extend({
     tagName: "div",
     showFolder: function(folder) {
@@ -353,6 +368,10 @@
     },
     showFile: function(file) {
       this.innerView = new FileView({ model: file });
+      this.render();
+    },
+    showDeleted: function() {
+      this.innerView = new DeletedView();
       this.render();
     },
     render: function() {
@@ -364,25 +383,30 @@
   $(function () {
     
     var fileTree = new FileTree();
-    var fileTreePane = new FileTreePane({
-      model: fileTree
-    });
     var notificationFeed = new NotificationFeed({
-      model: fileTree
+      getTree: function() { return fileTree.tree }
     });
     var mainPane = new MainPane();
-    fileTreePane.render();
-    $('#sidebar').append(fileTreePane.$el);
+    fileTree.render();
+    $('#sidebar').append(fileTree.$el);
     $('#main').append(mainPane.$el);
     notificationFeed.open();
     notificationFeed.on("event:load", function(struct) {
       mainPane.showFolder(_.defaults({}, _.first(struct).attributes));
     });
-    fileTreePane.on("folder:select", function(folder) {
+    fileTree.on("folder:select", function(folder) {
       mainPane.showFolder(folder);
     });
-    fileTreePane.on("file:select", function(file) {
+    fileTree.on("file:select", function(file) {
       mainPane.showFile(file);
+    });
+    notificationFeed.on("event:delete", function(struct) {
+      // Handle being on the deleted page already
+      if (_.isUndefined(mainPane.innerView.model)) return;
+      // If the current path has been deleted, then hide it.
+      if (mainPane.innerView.model.path == struct.attributes.path) {
+        mainPane.showDeleted();
+      }
     });
     
   });
