57e33189b4a3f87b14a7e741505e2836c926433e
Tim Dettrick <t.dettrick@uq.edu.au>
2013-05-09 10:15:04 +1000

    Integrating JaCoCo for test code coverage reporting.

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,8 @@
 import sbt._
 import Keys._
 import play.Project._
+import de.johoop.jacoco4sbt._
+import JacocoPlugin._
 
 object ApplicationBuild extends Build {
 
@@ -23,7 +25,14 @@ object ApplicationBuild extends Build {
     "be.objectify" %% "deadbolt-java" % "2.0-SNAPSHOT"
   )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
+  lazy val s = Defaults.defaultSettings ++ Seq(jacoco.settings:_*)
+
+  val main = play.Project(appName, appVersion, appDependencies, settings = s).settings(
+    // Play Framework can't do parallel testing
+    parallelExecution in jacoco.Config := false,
+    // Jacoco report output
+    jacoco.reportFormats in jacoco.Config := Seq(XMLReport("utf-8"), HTMLReport("utf-8"), CSVReport("utf-8")),
+    jacoco.outputDirectory in jacoco.Config := file("target/jacoco"),
     // Get rid of jBoss logging warning
     javaOptions := Seq("-Dorg.jboss.logging.provider=slf4j"),
     // Allows mock Http.Contexts to be built for play-authenticate
