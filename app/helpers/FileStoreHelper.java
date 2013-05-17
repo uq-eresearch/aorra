4cf441da036623267bd6f5ee988ca16bc9997b87
Tim Dettrick <t.dettrick@uq.edu.au>
2013-05-17 10:47:09 +1000

    Fixing mkdir tests (so they actually run!)

--- app/helpers/FileStoreHelper.java
+++ app/helpers/FileStoreHelper.java
@@ -7,6 +7,7 @@ import java.util.LinkedList;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 
+import play.Logger;
 import play.Play;
 import service.GuiceInjectionPlugin;
 import service.filestore.FileStore;
@@ -16,14 +17,21 @@ public class FileStoreHelper {
   public static class FileExistsException extends Exception {
     private static final long serialVersionUID = 1L;
     public FileExistsException(FileStore.File f) {
-      super(f.getPath());
+      super("File already exists: "+f.getPath());
     }
   }
 
   public static class FolderExistsException extends Exception {
     private static final long serialVersionUID = 1L;
     public FolderExistsException(FileStore.Folder f) {
-      super(f.getPath());
+      super("Folder already exists: "+f.getPath());
+    }
+  }
+
+  public static class FolderNotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
+    public FolderNotFoundException(final String name) {
+      super("Required folder does not exist: " + name);
     }
   }
 
@@ -34,7 +42,8 @@ public class FileStoreHelper {
   }
 
   public void mkdir(final String absPath, final boolean createParents)
-      throws FileExistsException, FolderExistsException, RepositoryException {
+      throws FileExistsException, FolderExistsException,
+      FolderNotFoundException, RepositoryException {
     final FileStore.Manager manager = fileStore().getManager(session);
     FileStore.Folder folder = manager.getRoot();
     final Iterator<String> iter = getPathParts(absPath).iterator();
@@ -44,8 +53,12 @@ public class FileStoreHelper {
       FileStore.FileOrFolder fof = folder.getFileOrFolder(name);
       if (fof == null) {
         // Only create if we can create parents or it's not a parent
-        if (createParents || iter.hasNext()) {
-          folder.createFolder(name);
+        if (createParents || !iter.hasNext()) {
+          Logger.debug(String.format("Creating new folder in %s: %s",
+              folder.getPath(), name));
+          folder = folder.createFolder(name);
+        } else {
+          throw new FolderNotFoundException(name);
         }
       } else if (fof instanceof FileStore.Folder) {
         folder = (FileStore.Folder) fof;
@@ -62,7 +75,8 @@ public class FileStoreHelper {
     final Deque<String> q = new LinkedList<String>();
     java.io.File f = new java.io.File(absPath);
     do {
-      q.addFirst(f.getName());
+      if (!f.getName().equals(""))
+        q.addFirst(f.getName());
       f = f.getParentFile();
     } while (f != null);
     return q;
--- test/helpers/FileStoreHelperTest.java
+++ test/helpers/FileStoreHelperTest.java
@@ -1,18 +1,24 @@
 package helpers;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.Fail.fail;
 import static play.test.Helpers.running;
 import static test.AorraTestUtils.fakeAorraApp;
 import static test.AorraTestUtils.fileStore;
 import static test.AorraTestUtils.sessionFactory;
 
+import java.io.ByteArrayInputStream;
+
 import javax.jcr.Session;
+
+import org.junit.Test;
 
 import play.libs.F;
 import service.filestore.FileStore;
 
 public class FileStoreHelperTest {
 
+  @Test
   public void mkdirSingle() {
     running(fakeAorraApp(), new Runnable() {
       @Override
@@ -42,6 +48,70 @@ public class FileStoreHelperTest {
     });
   }
 
+  @Test
+  public void mkdirSingleWhenFolderExists() {
+    running(fakeAorraApp(), new Runnable() {
+      @Override
+      public void run() {
+        sessionFactory().inSession(new F.Function<Session,Session>() {
+          @Override
+          public Session apply(Session session) throws Throwable {
+            final FileStoreHelper fileStoreHelper =
+                new FileStoreHelper(session);
+            final String absPath = "/foobar";
+            FileStore.Manager fm = fileStore().getManager(session);
+            {
+              final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath);
+              assertThat(fof).isNull();
+              fm.getRoot().createFolder("foobar");
+            }
+            try {
+              fileStoreHelper.mkdir(absPath, false);
+              fail("Should have thrown FolderExistsException");
+            } catch (FileStoreHelper.FolderExistsException e) {
+              assertThat(e.getMessage()).contains("foobar");
+            }
+            return null;
+          }
+
+        });
+      }
+    });
+  }
+
+  @Test
+  public void mkdirSingleWhenFileExists() {
+    running(fakeAorraApp(), new Runnable() {
+      @Override
+      public void run() {
+        sessionFactory().inSession(new F.Function<Session,Session>() {
+          @Override
+          public Session apply(Session session) throws Throwable {
+            final FileStoreHelper fileStoreHelper =
+                new FileStoreHelper(session);
+            final String absPath = "/foobar";
+            FileStore.Manager fm = fileStore().getManager(session);
+            {
+              final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath);
+              assertThat(fof).isNull();
+              fm.getRoot().createFile("foobar", "text/plain",
+                  new ByteArrayInputStream("Some content.".getBytes()));
+            }
+            try {
+              fileStoreHelper.mkdir(absPath, false);
+              fail("Should have thrown FolderExistsException");
+            } catch (FileStoreHelper.FileExistsException e) {
+              assertThat(e.getMessage()).contains("foobar");
+            }
+            return null;
+          }
+
+        });
+      }
+    });
+  }
+
+  @Test
   public void mkdirMultiple() {
     running(fakeAorraApp(), new Runnable() {
       @Override
@@ -57,9 +127,50 @@ public class FileStoreHelperTest {
               final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath);
               assertThat(fof).isNull();
             }
-            fileStoreHelper.mkdir(absPath, true);
+            try {
+              fileStoreHelper.mkdir(absPath, false);
+              fail("Should have thrown FolderNotFoundException");
+            } catch (FileStoreHelper.FolderNotFoundException e) {
+              assertThat(e.getMessage()).contains("foo");
+            }
+            return null;
+          }
+
+        });
+      }
+    });
+  }
+
+  @Test
+  public void mkdirMultipleWithParentCreation() {
+    running(fakeAorraApp(), new Runnable() {
+      @Override
+      public void run() {
+        sessionFactory().inSession(new F.Function<Session,Session>() {
+          @Override
+          public Session apply(Session session) throws Throwable {
+            final FileStoreHelper fileStoreHelper =
+                new FileStoreHelper(session);
+            final String absPath1 = "/foo/bar";
+            final String absPath2 = "/foo/baz";
+            FileStore.Manager fm = fileStore().getManager(session);
             {
-              final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath);
+              final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath1);
+              assertThat(fof).isNull();
+            }
+            fileStoreHelper.mkdir(absPath1, true);
+            {
+              final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath1);
+              assertThat(fof).isNotNull();
+              assertThat(fof).isInstanceOf(FileStore.Folder.class);
+            }
+            {
+              final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath2);
+              assertThat(fof).isNull();
+            }
+            fileStoreHelper.mkdir(absPath2, true);
+            {
+              final FileStore.FileOrFolder fof = fm.getFileOrFolder(absPath2);
               assertThat(fof).isNotNull();
               assertThat(fof).isInstanceOf(FileStore.Folder.class);
             }
