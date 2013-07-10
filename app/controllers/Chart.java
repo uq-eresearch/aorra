cbd4fe87e3673fbb8efd2c18f4a4263344fcebd3
Tim Dettrick <t.dettrick@uq.edu.au>
2013-07-10 11:28:11 +1000

    Splitting chart list and chart actions.

--- app/controllers/Chart.java
+++ app/controllers/Chart.java
@@ -48,138 +48,151 @@ import ereefs.spreadsheet.XlsxDataSource;
 @With(UncacheableAction.class)
 public class Chart extends SessionAwareController {
 
-    private final FileStore fileStore;
+  private final FileStore fileStore;
 
-    @Inject
-    public Chart(final JcrSessionFactory sessionFactory,
-            final Jcrom jcrom,
-            final CacheableUserProvider sessionHandler,
-            final FileStore fileStore) {
-      super(sessionFactory, jcrom, sessionHandler);
-      this.fileStore = fileStore;
+  @Inject
+  public Chart(final JcrSessionFactory sessionFactory, final Jcrom jcrom,
+      final CacheableUserProvider sessionHandler, final FileStore fileStore) {
+    super(sessionFactory, jcrom, sessionHandler);
+    this.fileStore = fileStore;
+  }
+
+  private String buildUrl(ereefs.charts.Chart chart, String format,
+      String[] path) throws UnsupportedEncodingException {
+    StringBuilder result = new StringBuilder();
+    result.append(chart.getDescription().getType().toString().toLowerCase());
+    result.append(".");
+    if (StringUtils.isNotBlank(format)) {
+      result.append(format);
+    } else {
+      result.append("png");
     }
+    result.append("?");
+    List<NameValuePair> qparams = new ArrayList<NameValuePair>();
+    for (Map.Entry<String, String> me : chart.getDescription().getProperties()
+        .entrySet()) {
+      qparams.add(new BasicNameValuePair(me.getKey(), me.getValue()));
+    }
+    for (String p : path) {
+      qparams.add(new BasicNameValuePair("path", p));
+    }
+    result.append(URLEncodedUtils.format(qparams, "UTF-8"));
+    return controllers.routes.Chart.chart(result.toString()).url();
+  }
 
-    private String buildUrl(ereefs.charts.Chart chart, String format, String[] path) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
-        result.append(chart.getDescription().getType().toString().toLowerCase());
-        result.append(".");
-        if(StringUtils.isNotBlank(format)) {
-            result.append(format);
+  private String getParameter(String key) {
+    String[] values = request().queryString().get(key);
+    return ((values != null) && (values.length > 0)) ? values[0] : null;
+  }
+
+  private List<DataSource> getDatasources(Session session, String[] paths)
+      throws Exception {
+    List<DataSource> result = Lists.newArrayList();
+    if (paths == null) {
+      return null;
+    }
+    final FileStore.Manager fm = fileStore.getManager(session);
+    for (String path : paths) {
+      FileStore.FileOrFolder fof = fm.getFileOrFolder(path);
+      if (fof instanceof FileStoreImpl.File) {
+        FileStoreImpl.File file = (FileStoreImpl.File) fof;
+        // Check this is an OpenXML document (no chance otherwise)
+        if (!file
+            .getMimeType()
+            .equals(
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
         } else {
-            result.append("png");
+          result.add(new XlsxDataSource(file.getData()));
         }
-        result.append("?");
-        List<NameValuePair> qparams = new ArrayList<NameValuePair>();
-        for(Map.Entry<String, String> me : chart.getDescription().getProperties().entrySet()) {
-            qparams.add(new BasicNameValuePair(me.getKey(), me.getValue()));
-        }
-        for(String p : path) {
-            qparams.add(new BasicNameValuePair("path", p));
-        }
-        result.append(URLEncodedUtils.format(qparams, "UTF-8"));
-        return controllers.routes.Chart.charts(result.toString()).url();
+      }
     }
+    return result;
+  }
 
-    private String getParameter(String key) {
-        String[] values = request().queryString().get(key);
-        return ((values != null) && (values.length > 0))?values[0]:null;
-    }
-
-    private List<DataSource> getDatasources(Session session, String[] paths) throws Exception {
-        List<DataSource> result = Lists.newArrayList();
-        if(paths == null) {
-            return null;
+  @SubjectPresent
+  public Result charts() {
+    return inUserSession(new F.Function<Session, Result>() {
+      @Override
+      public final Result apply(Session session) throws Exception {
+        List<DataSource> datasources = getDatasources(session, request()
+            .queryString().get("path"));
+        ChartFactory f = new ChartFactory(datasources);
+        String format = getParameter("format");
+        if (StringUtils.isBlank(format)) {
+          format = "png";
         }
-        final FileStore.Manager fm = fileStore.getManager(session);
-        for(String path : paths) {
-            FileStore.FileOrFolder fof = fm.getFileOrFolder(path);
-            if (fof instanceof FileStoreImpl.File) {
-                FileStoreImpl.File file = (FileStoreImpl.File) fof;
-                // Check this is an OpenXML document (no chance otherwise)
-                if (!file.getMimeType().equals(
-                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
-                } else {
-                    result.add(new XlsxDataSource(file.getData()));
-                }
-            }
+        List<ereefs.charts.Chart> charts = f.getCharts(request().queryString());
+        final ObjectNode json = Json.newObject();
+        final ArrayNode aNode = json.putArray("charts");
+        for (ereefs.charts.Chart chart : charts) {
+          ChartDescription desc = chart.getDescription();
+          final ObjectNode chartNode = Json.newObject();
+          chartNode.put("type", desc.getType().toString());
+          for (Map.Entry<String, String> me : desc.getProperties().entrySet()) {
+            chartNode.put(me.getKey(), me.getValue());
+          }
+          chartNode.put("url",
+              buildUrl(chart, format, request().queryString().get("path")));
+          aNode.add(chartNode);
         }
-        return result;
-    }
+        return ok(json).as("application/json; charset=utf-8");
+      }
+    });
+  }
 
-    @SubjectPresent
-    public Result charts(final String chart) {
-        return inUserSession(new F.Function<Session, Result>() {
-            @Override
-            public final Result apply(Session session) throws RepositoryException {
-                try {
-                    List<DataSource> datasources = getDatasources(
-                            session, request().queryString().get("path"));
-                    ChartFactory f = new ChartFactory(datasources);
-                    if(StringUtils.isBlank(chart)) {
-                        String format = getParameter("format");
-                        if(StringUtils.isBlank(format)) {
-                            format = "png";
-                        }
-                        List<ereefs.charts.Chart> charts = f.getCharts(request().queryString());
-                        final ObjectNode json = Json.newObject();
-                        final ArrayNode aNode = json.putArray("charts");
-                        for(ereefs.charts.Chart chart : charts) {
-                            ChartDescription desc = chart.getDescription();
-                            final ObjectNode chartNode = Json.newObject();
-                            chartNode.put("type", desc.getType().toString());
-                            for(Map.Entry<String, String> me : desc.getProperties().entrySet()) {
-                                chartNode.put(me.getKey(), me.getValue());
-                            }
-                            chartNode.put("url", buildUrl(chart, format, request().queryString().get("path")));
-                            aNode.add(chartNode);
-                        }
-                        return ok(json).as("application/json; charset=utf-8");
-                    } else {
-                        ChartType type = ChartType.getChartType(FilenameUtils.removeExtension(chart));
-                        if(type == null) {
-                            return notFound("unknown chart type "+chart);
-                        }
-                        String format = FilenameUtils.getExtension(chart);
-                        List<ereefs.charts.Chart> charts = f.getCharts(type, request().queryString());
-                        if(charts.isEmpty()) {
-                            return notFound();
-                        } else {
-                            ChartRenderer renderer = new ChartRenderer(charts.get(0).getChart());
-                            String svg = renderer.render();
-                            return toFormat(svg, format);
-                        }
-                    }
-                } catch(Exception e) {
-                    throw new RepositoryException(e);
-                }
-            }
-        });
-    }
-
-    private Result toFormat(String svg, String format) throws Exception {
-        if("png".equals(format)) {
-            Document doc = toDocument(svg);
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            new PNGTranscoder().transcode(new TranscoderInput(doc), new TranscoderOutput(os));
-            return ok(os.toByteArray()).as("image/png");
+  @SubjectPresent
+  public Result chart(final String chart) {
+    return inUserSession(new F.Function<Session, Result>() {
+      @Override
+      public final Result apply(Session session) throws Exception {
+        List<DataSource> datasources = getDatasources(session, request()
+            .queryString().get("path"));
+        ChartFactory f = new ChartFactory(datasources);
+        ChartType type = ChartType.getChartType(FilenameUtils
+            .removeExtension(chart));
+        if (type == null) {
+          return notFound("unknown chart type " + chart);
+        }
+        String format = FilenameUtils.getExtension(chart);
+        List<ereefs.charts.Chart> charts = f.getCharts(type, request()
+            .queryString());
+        if (charts.isEmpty()) {
+          return notFound();
         } else {
-            return ok(svg).as("image/svg+xml");
+          ChartRenderer renderer = new ChartRenderer(charts.get(0).getChart());
+          String svg = renderer.render();
+          return toFormat(svg, format);
         }
-    }
+      }
+    });
+  }
 
-    private Document toDocument(String svg) throws Exception {
-        // Turn back into DOM
-        String parserName = XMLResourceDescriptor.getXMLParserClassName();
-        SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parserName);
-        Document doc = f.createDocument("file:///test.svg",
-          new CharArrayReader(svg.toCharArray()));
-        String h = doc.getDocumentElement().getAttribute("height");
-        String w = doc.getDocumentElement().getAttribute("width");
-        doc.getDocumentElement().setAttributeNS(null, "viewbox", String.format("0 0 %s %s", w, h));
-//        if (relativeDimensions) {
-//          doc.getDocumentElement().setAttribute("height", "100%")
-//          doc.getDocumentElement().setAttribute("width", "100%")
-//        }
-        return doc;
+  private Result toFormat(String svg, String format) throws Exception {
+    if ("png".equals(format)) {
+      Document doc = toDocument(svg);
+      ByteArrayOutputStream os = new ByteArrayOutputStream();
+      new PNGTranscoder().transcode(new TranscoderInput(doc),
+          new TranscoderOutput(os));
+      return ok(os.toByteArray()).as("image/png");
+    } else {
+      return ok(svg).as("image/svg+xml");
     }
+  }
+
+  private Document toDocument(String svg) throws Exception {
+    // Turn back into DOM
+    String parserName = XMLResourceDescriptor.getXMLParserClassName();
+    SAXSVGDocumentFactory f = new SAXSVGDocumentFactory(parserName);
+    Document doc = f.createDocument("file:///test.svg",
+        new CharArrayReader(svg.toCharArray()));
+    String h = doc.getDocumentElement().getAttribute("height");
+    String w = doc.getDocumentElement().getAttribute("width");
+    doc.getDocumentElement().setAttributeNS(null, "viewbox",
+        String.format("0 0 %s %s", w, h));
+    // if (relativeDimensions) {
+    // doc.getDocumentElement().setAttribute("height", "100%")
+    // doc.getDocumentElement().setAttribute("width", "100%")
+    // }
+    return doc;
+  }
 }
--- test/controllers/ChartTest.java
+++ test/controllers/ChartTest.java
@@ -1,8 +1,10 @@
 package controllers;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
 import static play.test.Helpers.callAction;
 import static play.test.Helpers.charset;
+import static play.test.Helpers.contentAsString;
 import static play.test.Helpers.contentType;
 import static play.test.Helpers.header;
 import static play.test.Helpers.status;
@@ -30,10 +32,14 @@ public class ChartTest {
           final User user,
           final FakeRequest newRequest) throws Throwable {
         {
-          final Call call =
-              controllers.routes.Chart.charts("");
+          final Call call = controllers.routes.Chart.charts();
           assertThat(call.method()).isEqualTo("GET");
           assertThat(call.url()).isEqualTo("/charts/");
+        }
+        {
+          final Call call = controllers.routes.Chart.chart("marine");
+          assertThat(call.method()).isEqualTo("GET");
+          assertThat(call.url()).isEqualTo("/charts/marine");
         }
         return session;
       }
@@ -49,12 +55,30 @@ public class ChartTest {
           final User user,
           final FakeRequest newRequest) throws Throwable {
         final Result result = callAction(
-            controllers.routes.ref.Chart.charts(""),
+            controllers.routes.ref.Chart.charts(),
             newRequest);
         assertThat(status(result)).isEqualTo(200);
         assertThat(contentType(result)).isEqualTo("application/json");
         assertThat(charset(result)).isEqualTo("utf-8");
         assertThat(header("Cache-Control", result)).isEqualTo("no-cache");
+        assertThat(contentAsString(result)).isEqualTo("{\"charts\":[]}");
+        return session;
+      }
+    });
+  }
+
+  @Test
+  public void getChart() {
+    asAdminUser(new F.Function3<Session, User, FakeRequest, Session>() {
+      @Override
+      public Session apply(
+          final Session session,
+          final User user,
+          final FakeRequest newRequest) throws Throwable {
+        final Result result = callAction(
+            controllers.routes.ref.Chart.chart("marine"),
+            newRequest);
+        assertThat(status(result)).isEqualTo(404);
         return session;
       }
     });
