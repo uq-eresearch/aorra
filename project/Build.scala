33e9a688615e0faf2f037c81266949ba19ccc67f
Tim Dettrick <t.dettrick@uq.edu.au>
2013-08-07 15:33:27 +1000

    Removing unnecessary resolvers to speed up dependency resolution.

--- project/Build.scala
+++ project/Build.scala
@@ -70,9 +70,7 @@ object ApplicationBuild extends Build {
     //Keys.fork in (Test) := true,
     //javaOptions in (Test) += "-Xdebug",
     //javaOptions in (Test) += "-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=9998",
-    resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
     resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
-    resolvers += Resolver.url("play-authenticate (release)", url("http://joscha.github.com/play-authenticate/repo/releases/"))(Resolver.ivyStylePatterns),
     resolvers += Resolver.url("play-authenticate (snapshot)", url("http://joscha.github.com/play-authenticate/repo/snapshots/"))(Resolver.ivyStylePatterns)
   ).dependsOn(RootProject(uri(
       "git://github.com/schaloner/deadbolt-2-core.git"))
