f20674cdda7973c094abb1d80f9895109bacd6ef
Tim Dettrick <t.dettrick@uq.edu.au>
2013-08-23 09:59:40 +1000

    Tinkering with ChartBuilder prior to refactor.

--- app/charts/builder/ChartBuilder.java
+++ app/charts/builder/ChartBuilder.java
@@ -12,54 +12,50 @@ import com.google.common.collect.Lists;
 
 public class ChartBuilder {
 
-    private static List<ChartTypeBuilder> BUILDERS = new ImmutableList.Builder<ChartTypeBuilder>()
-            .add(new MarineSpreadsheetChartBuilder())
-            .add(new CotsOutbreakSpreadsheetBuilder())
-            .add(new AnnualRainfallChartBuilder())
-            .build();
+  private static List<ChartTypeBuilder> BUILDERS =
+    new ImmutableList.Builder<ChartTypeBuilder>()
+      .add(new MarineSpreadsheetChartBuilder())
+      .add(new CotsOutbreakSpreadsheetBuilder())
+      .add(new AnnualRainfallChartBuilder())
+      .build();
 
-    private List<DataSource> datasources;
+  private final List<DataSource> datasources;
 
-    public ChartBuilder() {
+  public ChartBuilder(List<DataSource> datasources) {
+    this.datasources = datasources;
+  }
+
+  public List<Chart> getCharts(ChartType type, Map<String, String[]> query) {
+    final List<Chart> result = Lists.newLinkedList();
+    for (final ChartTypeBuilder builder : BUILDERS) {
+      if (builder.canHandle(type, datasources)) {
+        result.addAll(builder.build(datasources, query));
+      }
     }
-
-    public ChartBuilder(List<DataSource> datasources) {
-        this.datasources = datasources;
-    }
-
-    public List<Chart> getCharts(ChartType type, Map<String, String[]> query) {
-        List<Chart> result = Lists.newArrayList();
-        for(ChartTypeBuilder builder : BUILDERS) {
-            if(builder.canHandle(type, datasources)) {
-                result.addAll(builder.build(datasources, query));
-            }
+    // make sure charts are sorted by region
+    // https://github.com/uq-eresearch/aorra/issues/44
+    Collections.sort(result, new Comparator<Chart>() {
+      @Override
+      public int compare(Chart c1, Chart c2) {
+        if (getRegion(c1) == null) {
+          if (getRegion(c2) == null)
+            return 0;
+          return -1;
+        } else {
+          return getRegion(c1).compareTo(getRegion(c2));
         }
-        // make sure charts are sorted by region 
-        // https://github.com/uq-eresearch/aorra/issues/44
-        Collections.sort(result, new Comparator<Chart>() {
-            @Override
-            public int compare(Chart c1, Chart c2) {
-                Region r1 = null;
-                Region r2 = null;
-                if(c1.getDescription()!=null) {
-                    r1 = c1.getDescription().getRegion();
-                }
-                if(c2.getDescription()!=null) {
-                    r2 = c2.getDescription().getRegion();
-                }
-                if(r1 != null) {
-                    return r1.compareTo(r2);
-                } else if(r2 == null){
-                    return 0;
-                } else {
-                    return -1;
-                }
-            }});
-        return result;
-    }
+      }
+      private Region getRegion(Chart c) {
+        if (c.getDescription() == null)
+          return null;
+        return c.getDescription().getRegion();
+      }
+    });
+    return result;
+  }
 
-    public List<Chart> getCharts(Map<String, String[]> query) {
-        return getCharts(null, query);
-    }
+  public List<Chart> getCharts(Map<String, String[]> query) {
+    return getCharts(null, query);
+  }
 
 }
