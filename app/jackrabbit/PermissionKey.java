d52750cc2dae412f2deb644e0687a219da8d34ab
Tim Dettrick <t.dettrick@uq.edu.au>
2013-10-30 16:06:43 +1000

    Removing crazy recursive import.

--- app/jackrabbit/PermissionKey.java
+++ app/jackrabbit/PermissionKey.java
@@ -4,8 +4,6 @@ import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.CompareToBuilder;
 
-import jackrabbit.PermissionKey;
-
 public final class PermissionKey implements Comparable<PermissionKey> {
 
     private final String workspace;
