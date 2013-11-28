3782f45a1776fa6ee6d378f21e0aaa840df7f672
Tim Dettrick <t.dettrick@uq.edu.au>
2013-11-28 11:19:34 +1000

    Storing original URL so we get redirected after login.

--- app/controllers/SubjectPresentAction.java
+++ app/controllers/SubjectPresentAction.java
@@ -8,7 +8,6 @@ import play.mvc.SimpleResult;
 import play.libs.F.Promise;
 import play.mvc.Action;
 import play.mvc.Http.Context;
-import play.mvc.Result;
 import providers.CacheableUserProvider;
 import service.GuiceInjectionPlugin;
 
@@ -19,6 +18,7 @@ public class SubjectPresentAction extends Action.Simple {
   @Override
   public Promise<SimpleResult> call(final Context ctx) throws Throwable {
     if (getUser(ctx) == null) {
+      PlayAuthenticate.storeOriginalUrl(ctx);
       return pure(redirect(PlayAuthenticate.getResolver().login()));
     }
     return delegate.call(ctx);
