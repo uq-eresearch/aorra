34f80fd5981b114cf1ac7411f89d02bfab7d8baf
Tim Dettrick <t.dettrick@uq.edu.au>
2014-01-14 15:50:49 +1000

    Removing no longer needed markdown processing for HTML archives.

--- app/html/XToHtml.java
+++ app/html/XToHtml.java
@@ -4,14 +4,12 @@ import org.apache.commons.lang3.StringUtils;
 
 public class XToHtml {
 
-    public String toHtml(String content, String mimetype) {
-        if(StringUtils.containsIgnoreCase(mimetype, "markdown")) {
-            return new MarkdownToHtml().toHtml(content);
-        } else if(StringUtils.containsIgnoreCase(mimetype, "html")) {
-            return content;
-        } else {
-            return null;
-        }
+  public String toHtml(String content, String mimetype) {
+    if (StringUtils.containsIgnoreCase(mimetype, "html")) {
+      return content;
+    } else {
+      return null;
     }
+  }
 
 }
