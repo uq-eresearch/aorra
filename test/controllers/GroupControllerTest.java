8c894ce234df8a456ad6d71bd2e6da16480e20da
Tim Dettrick <t.dettrick@uq.edu.au>
2014-01-21 17:23:47 +1000

    Adding more testing on GroupController.

--- test/controllers/GroupControllerTest.java
+++ test/controllers/GroupControllerTest.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.fail;
 import static play.test.Helpers.callAction;
 import static play.test.Helpers.charset;
 import static play.test.Helpers.contentAsString;
@@ -17,9 +18,11 @@ import java.io.ByteArrayInputStream;
 import java.util.List;
 import java.util.UUID;
 
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Session;
 
 import models.Flag;
+import models.GroupManager;
 import models.Notification;
 import models.User;
 import models.UserDAO;
@@ -29,6 +32,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.apache.jackrabbit.api.security.user.Group;
 import org.junit.Test;
 
 import play.api.mvc.Call;
@@ -107,6 +111,111 @@ public class GroupControllerTest {
         assertThat(json.has("id")).isTrue();
         assertThat(json.has("name")).isTrue();
         assertThat(json.has("members")).isTrue();
+        return session;
+      }
+    });
+  }
+
+  @Test
+  public void create() {
+    asAdminUser(new F.Function3<Session, User, FakeRequest, Session>() {
+      @Override
+      public Session apply(
+          final Session session,
+          final User user,
+          final FakeRequest newRequest) throws Throwable {
+        final ObjectNode jsonRequest = Json.newObject();
+        jsonRequest.put("name", "newgroup");
+        final Result result = callAction(
+            controllers.routes.ref.GroupController.create(),
+            newRequest.withJsonBody(jsonRequest));
+        assertThat(new GroupManager(session).find("newgroup")).isNotNull();
+        assertThat(status(result)).isEqualTo(201);
+        assertThat(contentType(result)).isEqualTo("application/json");
+        assertThat(charset(result)).isEqualTo("utf-8");
+        assertThat(header("Cache-Control", result))
+          .isEqualTo("max-age=0, must-revalidate");
+        final JsonNode json = Json.parse(contentAsString(result));
+        assertThat(json.isArray()).isFalse();
+        assertThat(json.has("id")).isTrue();
+        assertThat(json.has("name")).isTrue();
+        assertThat(json.has("members")).isTrue();
+        return session;
+      }
+    });
+  }
+
+  @Test
+  public void update() {
+    asAdminUser(new F.Function3<Session, User, FakeRequest, Session>() {
+      @Override
+      public Session apply(
+          final Session session,
+          final User user,
+          final FakeRequest newRequest) throws Throwable {
+        final GroupManager gm = new GroupManager(session);
+        final Group group = gm.create("newgroup");
+        {
+          final ObjectNode jsonRequest = Json.newObject();
+          jsonRequest.putArray("members").add(user.getId());
+          final Result result = callAction(
+              controllers.routes.ref.GroupController.update("newgroup"),
+              newRequest.withJsonBody(jsonRequest));
+          assertThat(gm.memberships(user.getJackrabbitUserId()))
+            .contains(group);
+          assertThat(status(result)).isEqualTo(200);
+          assertThat(contentType(result)).isEqualTo("application/json");
+          assertThat(charset(result)).isEqualTo("utf-8");
+          assertThat(header("Cache-Control", result))
+            .isEqualTo("max-age=0, must-revalidate");
+          final JsonNode json = Json.parse(contentAsString(result));
+          assertThat(json.isArray()).isFalse();
+          assertThat(json.has("id")).isTrue();
+          assertThat(json.has("name")).isTrue();
+          assertThat(json.has("members")).isTrue();
+        }
+        {
+          final ObjectNode jsonRequest = Json.newObject();
+          jsonRequest.putArray("members");
+          final Result result = callAction(
+              controllers.routes.ref.GroupController.update("newgroup"),
+              newRequest.withJsonBody(jsonRequest));
+          assertThat(gm.memberships(user.getJackrabbitUserId()))
+            .excludes(group);
+          assertThat(status(result)).isEqualTo(200);
+          assertThat(contentType(result)).isEqualTo("application/json");
+          assertThat(charset(result)).isEqualTo("utf-8");
+          assertThat(header("Cache-Control", result))
+            .isEqualTo("max-age=0, must-revalidate");
+          final JsonNode json = Json.parse(contentAsString(result));
+          assertThat(json.isArray()).isFalse();
+          assertThat(json.has("id")).isTrue();
+          assertThat(json.has("name")).isTrue();
+          assertThat(json.has("members")).isTrue();
+        }
+        return session;
+      }
+    });
+  }
+
+  @Test
+  public void delete() {
+    asAdminUser(new F.Function3<Session, User, FakeRequest, Session>() {
+      @Override
+      public Session apply(
+          final Session session,
+          final User user,
+          final FakeRequest newRequest) throws Throwable {
+        final Result result = callAction(
+            controllers.routes.ref.GroupController.delete("testgroup"),
+            newRequest);
+        try {
+          new GroupManager(session).find("testgroup");
+          fail();
+        } catch (PathNotFoundException e) {
+          // As expected
+        }
+        assertThat(status(result)).isEqualTo(204);
         return session;
       }
     });
