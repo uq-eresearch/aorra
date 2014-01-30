853813a82827139919013b9ea4657dac57fccc89
Andre Gebers <a.gebers@uq.edu.au>
2014-01-30 15:49:52 +1000

    minor

--- app/charts/builder/spreadsheet/MarineBarChartBuilder.java
+++ app/charts/builder/spreadsheet/MarineBarChartBuilder.java
@@ -8,7 +8,6 @@ import java.io.StringWriter;
 import java.util.List;
 import java.util.Map;
 
-import org.jfree.data.category.CategoryDataset;
 import org.supercsv.io.CsvListWriter;
 import org.supercsv.prefs.CsvPreference;
 
