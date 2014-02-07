2ee0de20c659199343b5e7c30bad431c46c05678
Andre Gebers <a.gebers@uq.edu.au>
2014-02-07 11:17:07 +1000

    only whole-numbers on the range axis, fixes #137

--- app/charts/graphics/CotsOutbreak.java
+++ app/charts/graphics/CotsOutbreak.java
@@ -2,6 +2,7 @@ package charts.graphics;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 
 import org.apache.commons.lang3.StringUtils;
@@ -11,6 +12,7 @@ import org.jfree.chart.axis.DateAxis;
 import org.jfree.chart.axis.DateTickUnit;
 import org.jfree.chart.axis.DateTickUnitType;
 import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.NumberTickUnit;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.xy.XYDataset;
@@ -43,6 +45,8 @@ public class CotsOutbreak {
         plot.setRangeCrosshairVisible(true);
         final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
         rangeAxis.setAutoRangeIncludesZero(true);
+        rangeAxis.setTickUnit(new NumberTickUnit(
+            (Math.round(Math.floor(getMaxOutbreaks(dataset)))/20)+1, new DecimalFormat("0")));
         XYItemRenderer r = plot.getRenderer();
         r.setSeriesPaint(0, ((seriesColor != null) && !Color.white.equals(seriesColor))?
             seriesColor:Color.blue);
@@ -51,6 +55,17 @@ public class CotsOutbreak {
         DateTickUnit unit = new DateTickUnit(DateTickUnitType.YEAR, 1, new SimpleDateFormat("yyyy"));
         axis.setTickUnit(unit);
         return new JFreeChartDrawable(chart, dimension);
+    }
+
+    public double getMaxOutbreaks(XYDataset dataset) {
+      double result = 0;
+      for(int i=0;i<dataset.getItemCount(0);i++) {
+        Number n = dataset.getY(0, i);
+        if(n!= null) {
+          result = Math.max(n.doubleValue(), result);
+        }
+      }
+      return result;
     }
 
 }
