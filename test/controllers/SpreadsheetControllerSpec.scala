94672eeecfaf1037a205d3f80995b7935a49bead
Tim Dettrick <t.dettrick@uq.edu.au>
2014-02-13 10:58:22 +1000

    Handing reverse proxy by allowing GET & HEAD checks of external cell references.

--- test/controllers/SpreadsheetControllerSpec.scala
+++ test/controllers/SpreadsheetControllerSpec.scala
@@ -6,8 +6,7 @@ import models.User
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeHeaders
 import play.api.test.FakeRequest
-import play.api.test.Helpers.HEAD
-import play.api.test.Helpers.POST
+import play.api.test.Helpers.{GET, HEAD, POST}
 import play.api.test.Helpers.OK
 import play.api.test.Helpers.contentType
 import play.api.test.Helpers.header
@@ -29,26 +28,32 @@ class SpreadsheetControllerSpec extends Specification {
 
   implicit val timeout = Timeout(30000)
 
+  val checkMethods = Seq(HEAD, GET)
+
   "spreadsheet external reference - check" >> {
 
     "requires login" in new FakeAorraApp {
       asAdminUser { (session: Session, user: User, rh: FakeHeaders) =>
         val id = UUID.randomUUID.toString
-        val Some(result) = route(
-            FakeRequest(HEAD, s"/file/$id/spreadsheet-external-references"))
-        status(result) must equalTo(303)
-        header("Location", result) must beSome("/login")
+        for (method <- checkMethods) {
+          val Some(result) = route(
+              FakeRequest(method, s"/file/$id/spreadsheet-external-references"))
+          status(result) must equalTo(303)
+          header("Location", result) must beSome("/login")
+        }
       }
     }
 
     "returns 404 if file doesn't exist" in new FakeAorraApp {
       asAdminUser { (session: Session, user: User, rh: FakeHeaders) =>
         val id = UUID.randomUUID.toString
-        val Some(result) = route(
-            FakeRequest(HEAD,
-                s"/file/$id/spreadsheet-external-references", rh,
-                AnyContentAsEmpty))
-        status(result) must equalTo(404)
+        for (method <- checkMethods) {
+          val Some(result) = route(
+              FakeRequest(method,
+                  s"/file/$id/spreadsheet-external-references", rh,
+                  AnyContentAsEmpty))
+          status(result) must equalTo(404)
+        }
       }
     }
 
@@ -58,11 +63,13 @@ class SpreadsheetControllerSpec extends Specification {
           .createFile("test.txt", "text/plain",
             new ByteArrayInputStream("Some content".getBytes))
         val id = file.getIdentifier
-        val Some(result) = route(
-            FakeRequest(HEAD,
-                s"/file/$id/spreadsheet-external-references", rh,
-                AnyContentAsEmpty))
-        status(result) must equalTo(404)
+        for (method <- checkMethods) {
+          val Some(result) = route(
+              FakeRequest(method,
+                  s"/file/$id/spreadsheet-external-references", rh,
+                  AnyContentAsEmpty))
+          status(result) must equalTo(404)
+        }
       }
     }
 
@@ -72,11 +79,13 @@ class SpreadsheetControllerSpec extends Specification {
           .createFile("test.xlsx", XLSX_MIME_TYPE,
             new FileInputStream("test/cots_outbreak.xlsx"))
         val id = file.getIdentifier
-        val Some(result) = route(
-            FakeRequest(HEAD,
-                s"/file/$id/spreadsheet-external-references", rh,
-                AnyContentAsEmpty))
-        status(result) must equalTo(404)
+        for (method <- checkMethods) {
+          val Some(result) = route(
+              FakeRequest(method,
+                  s"/file/$id/spreadsheet-external-references", rh,
+                  AnyContentAsEmpty))
+          status(result) must equalTo(404)
+        }
       }
     }
 
@@ -86,11 +95,13 @@ class SpreadsheetControllerSpec extends Specification {
           .createFile("test.xlsx", XLSX_MIME_TYPE,
             new FileInputStream("test/extref.xlsx"))
         val id = file.getIdentifier
-        val Some(result) = route(
-            FakeRequest(HEAD,
-                s"/file/$id/spreadsheet-external-references", rh,
-                AnyContentAsEmpty))
-        status(result) must equalTo(200)
+        for (method <- checkMethods) {
+          val Some(result) = route(
+              FakeRequest(method,
+                  s"/file/$id/spreadsheet-external-references", rh,
+                  AnyContentAsEmpty))
+          status(result) must equalTo(200)
+        }
       }
     }
 
