fb57f03f8ca84025fd7ebd958e28cd20073c167e
Tim Dettrick <t.dettrick@uq.edu.au>
2014-05-30 11:49:21 +1000

    Fixing another missing file in the manifest.

--- test/controllers/InfographicControllerSpec.scala
+++ test/controllers/InfographicControllerSpec.scala
@@ -19,33 +19,82 @@ import scala.concurrent.Await
 import akka.util.Timeout
 import java.util.UUID
 import scala.io.Source
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.filefilter._
+import java.io.File
+import scala.collection.JavaConversions._
+import java.nio.file.Path
 
 class InfographicControllerSpec extends Specification {
 
   implicit val timeout = Timeout(30000)
 
-  "infographic data.json" >> {
-    "requires login" in new FakeAorraApp {
-      asAdminUser { (session: Session, user: User, rh: FakeHeaders) =>
-        val id = UUID.randomUUID.toString
-        val Some(result) = route(
-            FakeRequest(GET, s"/file/$id/infographic/data.json"))
-        status(result) must equalTo(303)
-        header("Location", result) must beSome("/login")
-      }
+  "data.json requires login" >> new FakeAorraApp {
+    asAdminUser { (session: Session, user: User, rh: FakeHeaders) =>
+      val id = UUID.randomUUID.toString
+      val Some(result) = route(
+          FakeRequest(GET, s"/file/$id/infographic/data.json"))
+      status(result) must equalTo(303)
+      header("Location", result) must beSome("/login")
     }
   }
 
-  "infographic static files" >> {
-    "should exist for every entry in manifest.txt" in new FakeAorraApp {
-      val manifest = Source.fromInputStream(
-        getClass.getResourceAsStream("/infographic/manifest.txt")).getLines
-      val id = UUID.randomUUID.toString
-      manifest.foreach { staticFile =>
-        val Some(result) = route(
-            FakeRequest(GET, s"/file/$id/infographic/$staticFile"))
-        status(result) must equalTo(200).setMessage(s"$staticFile should exist")
+  "static files should exist for every entry in manifest.txt" >> new FakeAorraApp {
+    val manifest = Source.fromInputStream(
+      getClass.getResourceAsStream("/infographic/manifest.txt")).getLines
+    val id = UUID.randomUUID.toString
+    manifest.foreach { staticFile =>
+      val Some(result) = route(
+          FakeRequest(GET, s"/file/$id/infographic/$staticFile"))
+      status(result) must equalTo(200).setMessage(s"$staticFile should exist")
+    }
+  }
+
+  "static files should be in the manifest if they're" >> {
+
+    lazy val inManifest: Path => Boolean = {
+      val manifest: Set[String] = Source.fromInputStream(
+        getClass.getResourceAsStream("/infographic/manifest.txt"))
+          .getLines
+          .toSet;
+      { path: Path =>
+        manifest.contains(path.toString)
       }
+    }
+
+    def resourceFilesByRegex(re: String): List[Path] = {
+      val baseDir = new File("./resources/infographic")
+      val files = FileUtils.listFiles(
+          baseDir,
+          new RegexFileFilter(re),
+          TrueFileFilter.INSTANCE).toList
+      files.map { file =>
+        baseDir.toPath.relativize(file.toPath)
+      }
+    }
+
+    "css" >> {
+      resourceFilesByRegex(""".*\.css""").forall(inManifest) must beTrue
+    }
+
+    "geojson" >> {
+      resourceFilesByRegex(""".*\.geojson""").forall(inManifest) must beTrue
+    }
+
+    "html" >> {
+      resourceFilesByRegex(""".*\.html""").forall(inManifest) must beTrue
+    }
+
+    "js" >> {
+      resourceFilesByRegex(""".*\.js""").forall(inManifest) must beTrue
+    }
+
+    "png" >> {
+      resourceFilesByRegex(""".*\.png""").forall(inManifest) must beTrue
+    }
+
+    "svg" >> {
+      resourceFilesByRegex(""".*\.png""").forall(inManifest) must beTrue
     }
   }
 
